6ed10b9999e2761303f9b65cefa55cbae3485cbf
Laxminarayan Majhi <laxminarayanmajhi@Laxminarayans-Mac-mini.local>
2025-11-13 11:58:48 +0530

    removed typscript check during build.

--- app/news/[category]/page.tsx
+++ app/news/[category]/page.tsx
@@ -5,6 +5,8 @@ import { getArticles } from "@/common/utils";
 
 export const dynamic = "force-dynamic"; 
 
+
+
 export default async function CategoryPage({ params }: {params: string}) {
   const { category } = await params;
   const articles = await getArticles(category);
